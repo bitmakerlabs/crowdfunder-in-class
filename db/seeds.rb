1414665c24eb5233bcc36a8fd3582db0390c4c40
Paul Dowman <paul@pauldowman.com>
2013-04-09 09:29:07 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Project.destroy_all
+
+user = User.create!(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "jdoe111")
+
+50.times do |i|
+    project1 = user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}",
+        description: "description #{i}", goal: 13000)
+end
