77b766aacf2056085d216d755c71e709e48d2741
Paul Dowman <paul@pauldowman.com>
2013-04-09 08:23:55 -0400

    Generate new Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdfunderInClass::Application.config.secret_token = '8513daeb97f3617befc6085099e78827a804aae101eb26c3bd56d7c384f74e9912ca73abeb0427e589c93cec16877af1361ed5b10419faf3cc7f4dca4f6445e5'
