c31e05354c3f2c008402aa22771112b02c2c3532
Khurram Virani <kvirani@gmail.com>
2013-04-11 10:59:09 -0400

    Update seeds so that my/projects shows a smaller set of projects when logged in as Khurram Virani

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,14 @@ Project.destroy_all
 user = User.create!(first_name: "John", last_name: "Doe", email: "john@doe.com", password: "jdoe111")
 
 50.times do |i|
-    project1 = user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}",
+    user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}",
         description: "description #{i}", goal: 13000)
 end
+
+user2 = User.create!(first_name: "Khurram", last_name: "Virani", email: "kvirani@bitmakerlabs.com", password: "kviranikvirani")
+5.times do |i|
+    user.projects.create!(title: "Project #{i}", teaser: "Teaser text #{i}",
+        description: "description #{i}", goal: 13000)
+end
+
+
